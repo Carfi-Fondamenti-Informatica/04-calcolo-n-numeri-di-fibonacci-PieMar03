f2ba4ad2bb8a521cab077141e32284dfb921f2da
PieMar03 <115477227+PieMar03@users.noreply.github.com>
2022-11-23 15:40:55 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,12 +6,14 @@ int main() {
     int n;
     cin >> n;
     int i=2;
+    
     if (n>=2){
         cout << 1 << endl;
         while (i++ <=n){
             cout << PN + SN << endl;
             if (PN < SN)
                 PN += SN;
+            else SN += PN;
         }
     } else {
         cout << "errore" << endl;
