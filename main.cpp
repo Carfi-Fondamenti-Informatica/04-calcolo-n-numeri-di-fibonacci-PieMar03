b32d04088bbcb5ddfe79554521c8fc2d1d85c89f
PieMar03 <115477227+PieMar03@users.noreply.github.com>
2022-11-23 15:39:52 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-   return 0;
+    int PN=0;
+    int SN=1;
+    int n;
+    cin >> n;
+    int i=2;
+    if (n>=2){
+        cout << 1 << endl;
+        while (i++ <=n){
+            cout << PN + SN << endl;
+            if (PN < SN)
+                PN += SN;
+        }
+    } else {
+        cout << "errore" << endl;
+    }
+    return 0;
 }
